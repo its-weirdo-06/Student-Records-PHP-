16631acf32b466120e81207b3f09ef744202239b
Prasad Badhe <83410561+its-weirdo-06@users.noreply.github.com>
2021-05-24 15:42:45 +0530

    Add files via upload

---
+++ Slip2.php
@@ -0,0 +1,25 @@
+<?php
+  $a=$_GET['a'];
+  if(preg_match("^BC|A|S([1-3])([0-9][0-9][0-9])$^",$a))
+  {
+    echo"Roll no = ".substr($a,3)."<br>";
+      $year=substr($a,2,1);
+    if($year=='1')
+        echo"Year = 1st <br>";
+    if($year=='2')
+        echo"Year = 2nd <br>";
+    if($year=='3')
+        echo"Year = 3rd <br>";
+      $fac=substr($a,0,2);
+    if($fac=='BA')
+        echo"Faculty = BA";
+    if($fac=='BC')
+        echo"Faculty = BCA";
+    if($fac=='BS')
+        echo"Faculty = B.Sc.";
+  }
+  else
+  {
+  echo"Invalid ";
+  }
+?>
